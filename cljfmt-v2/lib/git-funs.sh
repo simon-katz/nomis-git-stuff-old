dc9a3d4e5ad0f0aeed95f2275688c31b67e5f8e8
Simon Katz <sk@nomistech.com>
2018-12-23 22:04:17 +0000

    cljfmt-v2 -- Add more functions

--- cljfmt-v2/lib/git-funs.sh
+++ cljfmt-v2/lib/git-funs.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-function git_stash_include_untracked {
+function git_stash_if_dirty_include_untracked {
     local message=$1
     git stash push \
         --quiet \
         --include-untracked \
         --message "${message}"
+}
+
+function replace_previous_n_commits {
+    local n=$1
+    local commit_sha=$2
+    echo "Committing: Replacing last ${n} commits with a single commit."
+    git reset --quiet --soft HEAD~${n}
+    git add . # TODO Is this needed?
+    git commit --quiet --no-verify -C ${commit_sha}
 }
