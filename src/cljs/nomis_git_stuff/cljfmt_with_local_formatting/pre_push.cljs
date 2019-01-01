7ddea2443a69597bfcc5a918f3fba11793f327d6
Simon Katz <sk@nomistech.com>
2019-01-01 15:51:08 +0000

    CLJS WIP: Check unpushed commit names

--- src/cljs/nomis_git_stuff/cljfmt_with_local_formatting/pre_push.cljs
+++ src/cljs/nomis_git_stuff/cljfmt_with_local_formatting/pre_push.cljs
@@ -1,46 +1,67 @@
 (ns nomis-git-stuff.cljfmt-with-local-formatting.pre-push
-  (:require [goog.string :as gstring]
+  (:require [clojure.string :as str]
+            [goog.string :as gstring]
             [goog.string.format]
             [planck.core :as core]
             [nomis-git-stuff.common.git :as git]))
+
+(defn exit-with-error
+  ([msg]
+   (exit-with-error msg 1))
+  ([msg status-code]
+   (println "Error:" msg) ; Can we write to stderr?
+   (core/exit status-code)))
+
+(defn local-formatting-commit-message? [s]
+  (str/starts-with? s "apply-local-formatting"))
 
 (defn pre-push []
 
   (println "command line args =" cljs.core/*command-line-args*)
 
   (let [[remote-name remote-location] cljs.core/*command-line-args*]
+
     (println "remote-name =" remote-name)
     (println "remote-location =" remote-location)
+
+    (println (gstring/format "branch-name = \"%s\""
+                             (git/branch-name)))
+    (println (gstring/format "top-stash-name = \"%s\""
+                             (git/top-stash-name)))
+    (println (gstring/format "commit message #1 = \"%s\""
+                             (git/top-commit-message 1)))
+    (println (gstring/format "commit message #2 = \"%s\""
+                             (git/top-commit-message 2)))
+    (println (gstring/format "commit message #3 = \"%s\""
+                             (git/top-commit-message 3)))
+    (println (gstring/format "safekeeping-stash-name = \"%s\""
+                             (git/safekeeping-stash-name "the-kind"
+                                                         "the-type"
+                                                         "the-commit-sha")))
+
+    (println "Hello World!")
+
+    (println "stdin =" (core/slurp core/*in*))
+
+    ;; TODO If there's nothing to push, you won't get any lines on stdin.
+    ;;      See https://stackoverflow.com/questions/22585091/git-hooks-pre-push-script-does-not-receive-input-via-stdin
+
+    (println (git/bash "git remote"))
+
+    (println "________________________________________")
+
     (assert (= remote-name "origin") ; TODO Do you need this check?
             "ERROR: This only works when there is a single remote and it is named \"origin\".")
     (let [unpushed-commit-names (git/unpushed-commit-names remote-name)]
       (println (gstring/format "unpushed-commit-names = %s"
-                               unpushed-commit-names))))
+                               unpushed-commit-names))
+      (when (some local-formatting-commit-message?
+                  unpushed-commit-names)
+        (exit-with-error
+         (str/join " "
+                   ["Cannot push an \"apply-local-formatting\" commit."
+                    "You may want the \"nomis-cljfmt-v2-git-push\" command."]))))
 
-  (println (gstring/format "branch-name = \"%s\""
-                           (git/branch-name)))
-  (println (gstring/format "top-stash-name = \"%s\""
-                           (git/top-stash-name)))
-  (println (gstring/format "commit message #1 = \"%s\""
-                           (git/top-commit-message 1)))
-  (println (gstring/format "commit message #2 = \"%s\""
-                           (git/top-commit-message 2)))
-  (println (gstring/format "commit message #3 = \"%s\""
-                           (git/top-commit-message 3)))
-  (println (gstring/format "safekeeping-stash-name = \"%s\""
-                           (git/safekeeping-stash-name "the-kind"
-                                                       "the-type"
-                                                       "the-commit-sha")))
+    (core/exit 1) ; TODO for now
 
-  (println "Hello World!")
-
-  (println "stdin =" (core/slurp core/*in*))
-
-  ;; TODO If there's nothing to push, you won't get any lines on stdin.
-  ;;      See https://stackoverflow.com/questions/22585091/git-hooks-pre-push-script-does-not-receive-input-via-stdin
-
-  (println (git/bash "git remote"))
-
-  (core/exit 1)
-
-  (println "SHOULDN'T SEE THIS."))
+    (println "SHOULDN'T SEE THIS.")))
